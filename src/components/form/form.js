f6cbde3006af050db6e43f4381668f78fe14eafe
Angel Diaz <angel.diaz@strongmind.com>
2021-09-28 05:21:42 -0700

    finished up workshop

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './form.css';
+import './form.scss';
 
 class Form extends Component {
   state = {
@@ -33,22 +33,26 @@ class Form extends Component {
   render() {
     return (
         <div className="form">
-          <>
+          <div className="input-form">
             <label htmlFor="name">name:</label>
             <input type="text" id="name" onChange={(e) => {this.onChange(e, "name")}}/>
             <label htmlFor="imageUrl">image url:</label>
             <input type="text" id="imageUrl" onChange={(e) => {this.onChange(e, "imageUrl")}}/>
             <label htmlFor="recipeUrl">recipe url:</label>
             <input type="text" id="recipeUrl" onChange={(e) => {this.onChange(e, "recipeUrl")}}/>
-            <button onClick={this.onSubmit}>submittt</button>
-          </>
-          { JSON.stringify(this.state.displayObject) }
-          {/* <div className="display-new-image">
-            <p>{this.state.displayObject.name}</p>
+            <button onClick={this.onSubmit}>SUBMIT</button>
+          </div>
+          {/* { JSON.stringify(this.state.displayObject) } */}
+          <div className="display-new-image">
+            {this.state.displayObject.name && 
+            <span>
+              <p>{this.state.displayObject.name}</p>
             <a href={this.state.displayObject.recipeUrl}>
               <img alt='most recent entry' src={this.state.displayObject.imageUrl}/>
             </a>
-          </div> */}
+            </span>
+            }
+          </div>
         </div>
   )}
 }
